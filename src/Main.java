c3891f1b3a947f902b22e18faaadae73efaf1c5f
SoHailaNekHat <sohaila.wb@gmail.com>
2022-10-11 21:05:19 +0430

    wrapper classes

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,17 @@ System.out.println(s.m1(80));
 //Association
 faculty uni=new faculty();
 uni.nameoftheuni();
-
+//wrapper classes
+        //int to Integer
+        Integer a=new Integer(10);
+       System.out.println(a+" is an Integer in wrapper class");
+        //char =Character
+        Character ch=new Character('C');
+        //short =Short
+        //long=Long
+        //float=Float
+        //double=Double
+        //boolean to Boolean
+        //byte=Byte
     }
 }
