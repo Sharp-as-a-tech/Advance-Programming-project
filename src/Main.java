8bda5e72f8b74e1a716aad7962fa0f749e0d72fd
SoHailaNekHat <sohaila.wb@gmail.com>
2022-10-11 21:16:23 +0430

    Autoboxing and unboxing are done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -18,5 +20,20 @@ uni.nameoftheuni();
         //double=Double
         //boolean to Boolean
         //byte=Byte
+
+
+
+        //Autoboxing
+        char c='c';
+        Character A = c;
+
+        System.out.println(A);
+        ArrayList<Integer> arrayList=new ArrayList<Integer>();
+        arrayList.add(25);
+        System.out.println(arrayList.get(0));
+        //unboxing
+        Character Char='a';
+        char cha=Char;
+        System.out.println(cha);
     }
 }
