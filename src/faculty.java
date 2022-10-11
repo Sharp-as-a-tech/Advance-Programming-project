d89d2ffc382501f3f86e32aa0d47b178a9655c47
SoHailaNekHat <sohaila.wb@gmail.com>
2022-10-11 20:43:24 +0430

    Associotion:Composition is done

--- src/faculty.java
+++ src/faculty.java
@@ -1,8 +1,15 @@
 public class faculty {
+public class students{
 
+    public void setName(){
+        University a=new University();
+        a.setName("Kandahar University");
+        System.out.println(a.getName());
+    }
+};
 public void nameoftheuni(){
-    University a=new University();
-    a.setName("Kandahar University");
-    System.out.println(a.getName());
+
+students s=new students();
+s.setName();
 }
 }
